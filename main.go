fb95261c39a31f65c4b182f76cad5d414db2de36
Lassiter Gregg <lassiter@users.noreply.github.com>
2019-01-02 00:13:51 -0600

    added msg values

--- main.go
+++ main.go
@@ -23,5 +23,17 @@ func main() {
      authToken := os.Getenv("AUTH_TOKEN")
      urlStr := "https://api.twilio.com/2010-04-01/Accounts/"   accountSid   "/Messages.json"
      
+    msgData := url.Values{}
+
+    msgData.Set("To","NUMBER_TO")
+
+    msgData.Set("From","NUMBER_FROM")
+
+    msgData.Set("Body",quotes[rand.Intn(len(quotes))])
+
+   msgDataReader := *strings.NewReader(msgData.Encode())
+   
+   
+     
 
 }
